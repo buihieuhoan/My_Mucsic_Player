8e39502b985fe6745f7b015b818ed51909c3e136
buihieuhoan <109495842+buihieuhoan@users.noreply.github.com>
2022-08-10 10:54:39 +0700

    Update music_list.js

--- music_list.js
+++ music_list.js
@@ -2,68 +2,68 @@ const allMusic = [
     {
         name: "Arena of valor SS9",
         author: "No infomation",
-        src: "Arena_SS9.mp3",
-        img: "Arena_SS9_2.jpg"
+        src: "./music_list/Arena_SS9.mp3",
+        img: "./assets/image/song_img/Arena_SS9_2.jpg"
       },
       {
         name: "Arena of valor SS17",
         author: "No infomation",
-        src: "Arena_SS17.mp3",
-        img: "Arena_SS17.jpg"
+        src: "./music_list/Arena_SS17.mp3",
+        img: "./assets/image/song_img/Arena_SS17.jpg"
       },
      
       {
         name: "Anh vẫn ở đây lofi",
         author: "Thành Đạt",
-        src: "Anh Vẫn Ở Đây  Lofi.mp3",
-        img: "anhvanoday.jpg"
+        src: "./music_list/Anh Vẫn Ở Đây  Lofi.mp3",
+        img: "./assets/image/song_img/anhvanoday.jpg"
       },
       {
         name: "Cảm ơn vì tất cả lofi",
         author: "Anh Quân",
-        src: "Cảm Ơn Vì Tất Cả Lofi.mp3",
-        img: "camonvitatca.jpg"
+        src: "./music_list/Cảm Ơn Vì Tất Cả Lofi.mp3",
+        img: "./assets/image/song_img/camonvitatca.jpg"
       },
       {
         name: "Chỉ bằng cái gật đầu",
         author: "Yan Nguyễn",
-        src: "Chỉ Bằng Cái Gật Đầu Lofi.mp3",
-        img: "chibangcaigatdau.jpg"
+        src: "./music_list/Chỉ Bằng Cái Gật Đầu Lofi.mp3",
+        img: "./assets/image/song_img/chibangcaigatdau.jpg"
       },
 
       {
         name: "Chỉ muốn bên anh thật gần",
         author: "Y Ling",
-        src: "Chỉ Muốn Bên Em Thật Gần Lofi.mp3",
-        img: "chimuonbenanhthatgan.jpg"
+        src: "./music_list/Chỉ Muốn Bên Em Thật Gần Lofi.mp3",
+        img: "./assets/image/song_img/chimuonbenanhthatgan.jpg"
       },
 
       {
         name: "Homura",
         author: "Lisa",
-        src: "Homura.mp3",
-        img: "homura.jpg"
+        src: "./music_list/Homura.mp3",
+        img: "./assets/image/song_img/homura.jpg"
       },
 
       {
         name: "Sao mình chưa nắm tay nhau",
         author: "Yan Nguyễn",
-        src: "Sao Mình Chưa Năm Tay Nhau lofi.mp3",
-        img: "saominhchuanamtaynhau.jpg"
+        src: "./music_list/Sao Mình Chưa Năm Tay Nhau lofi.mp3",
+        img: "./assets/image/song_img/saominhchuanamtaynhau.jpg"
       },
 
       {
         name: "Sold out",
         author: "Hawk Nelson",
-        src: "Sold Out.mp3",
-        img: "soldout.jpg"
+        src: "./music_list/Sold Out.mp3",
+        img: "./assets/image/song_img/soldout.jpg"
       },
 
       {
         name: "Trap Queen",
         author: "Adriana Gomez",
-        src: "Trap Queen .mp3",
-        img: "trapqueen.jpg"
+        src: "./music_list/Trap Queen .mp3",
+        img: "./assets/image/song_img/trapqueen.jpg"
       }
 ]
 
